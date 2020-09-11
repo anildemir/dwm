adb954f41756abebe6e07c30f0e4e3c2a1c94629
Anil Demir <anildemir@riseup.net>
2020-09-11 21:05:24 +0300

    increase font size

--- config.h
+++ config.h
@@ -12,8 +12,8 @@ static const unsigned int gappov    = 10;       /* vert outer gap between window
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Fira Code:pixelsize=10:antialias=true:autohint=true","JoyPixels:pixelsize=10:antialias=true:autohint=true"};
-static const char dmenufont[]       = "Fira Code:size=10";
+static const char *fonts[]          = { "Fira Code:pixelsize=12:antialias=true:autohint=true","JoyPixels:pixelsize=12:antialias=true:autohint=true"};
+static const char dmenufont[]       = "Fira Code:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
