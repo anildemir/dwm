8a154a59a7b5ecb875e62802822e07d34328d254
Anil Demir <anildemir@riseup.net>
2020-09-13 22:43:38 +0300

    update color scheme, some keybindings

--- config.h
+++ config.h
@@ -6,31 +6,35 @@
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
+static const unsigned int gappiv    = 20;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
 static const int swallowfloating    = 0;
 static const int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Fira Code:pixelsize=12:antialias=true:autohint=true","JoyPixels:pixelsize=12:antialias=true:autohint=true"};
+static const char *fonts[]          = { "Fira Code:pixelsize=12:antialias=true:autohint=true",
+                                        "JoyPixels:pixelsize=12:antialias=true:autohint=true"};
 static const char dmenufont[]       = "Fira Code:size=12";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#5190ff";
+static const char normbg[]          = "#24292e";
+static const char normbd[]          = "#5190ff";
+static const char normfg[]          = "#f6f8fa";
+static const char selfg[]           = "#24292e";
+static const char selbd[]           = "#abe885";
+static const char selbg[]           = "#85e89d";
+static const char titlefg[]         = "#f6f8fa";
+static const char titlebd[]         = "#5190ff";
+static const char titlebg[]         = "#5190ff";
 
 static const char *colors[][3]      = {
 
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
-	[SchemeTitle]  = { col_gray4, col_cyan,  col_cyan  },
+    [SchemeNorm]   = { normfg, normbg, normbd },
+    [SchemeSel]    = { selfg, selbg,  selbd  },
+    [SchemeTitle]  = { titlefg, titlebd,  titlebg  },
 };
 
 static const Bool viewontag         = True;     /* Switch view on tag switch */
-
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
@@ -45,9 +49,7 @@ static const Rule rules[] = {
 	{ "Gimp",    NULL,     NULL,           0,         1,          0,           0,        -1 },
 	{ "Firefox", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
 	{ "st-256color", NULL, NULL,           0,         0,          1,           0,        -1 },
-	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /*
-                                                                                                  xev
-                                                                                                  */
+    { NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 },
 };
 
 /* layout(s) */
@@ -76,13 +78,36 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn",
+    dmenufont, "-nb", normbg, "-nf", normfg, "-sb", selbd, "-sf", selfg, NULL
+};
 static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
+	{ MODKEY,                       XK_q,      killclient,     {0} },
+	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY,		            	XK_w,	   spawn,		   SHCMD("$BROWSER") },
+	{ MODKEY|ShiftMask,				XK_w,	   spawn,		   SHCMD("st -e iwctl") },
+	{ MODKEY,						XK_e,	   spawn,		   SHCMD("st -e neomutt") },
+	{ MODKEY,						XK_r,      spawn,          SHCMD("st -e lf") },
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
+    { MODKEY,	                	XK_Escape, spawn,          SHCMD("togglekb") },
+
+	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} }, /* tile */
+	{ MODKEY|ShiftMask,             XK_t,      setlayout,      {.v = &layouts[3]} }, /* 3-column */
+	{ MODKEY,						XK_y,      setlayout,      {.v = &layouts[2]} }, /* monocle */
+	{ MODKEY|ShiftMask,             XK_y,      setlayout,      {.v = &layouts[1]} }, /* floating */
+	{ MODKEY,						XK_Tab,    view,		   {0} },
+
+	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
+	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
+
+    { MODKEY,                       XK_a,      togglegaps,     {0} },
+    { MODKEY|ShiftMask,             XK_a,      defaultgaps,    {0} },
+    //{ MODKEY,						XK_s,      togglesticky,   {0} },
+
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -96,9 +121,7 @@ static Key keys[] = {
 	{ MODKEY|Mod4Mask|ShiftMask,    XK_l,      incrogaps,      {.i = -1 } },
 	{ MODKEY|Mod4Mask|ControlMask,  XK_h,      incrigaps,      {.i = +1 } },
 	{ MODKEY|Mod4Mask|ControlMask,  XK_l,      incrigaps,      {.i = -1 } },
-	{ MODKEY|Mod4Mask,              XK_0,      togglegaps,     {0} },
-	{ MODKEY|Mod4Mask|ShiftMask,    XK_0,      defaultgaps,    {0} },
-	{ MODKEY,                       XK_y,      incrihgaps,     {.i = +1 } },
+    { MODKEY,                       XK_y,      incrihgaps,     {.i = +1 } },
 	{ MODKEY,                       XK_o,      incrihgaps,     {.i = -1 } },
 	{ MODKEY|ControlMask,           XK_y,      incrivgaps,     {.i = +1 } },
 	{ MODKEY|ControlMask,           XK_o,      incrivgaps,     {.i = -1 } },
@@ -106,18 +129,12 @@ static Key keys[] = {
 	{ MODKEY|Mod4Mask,              XK_o,      incrohgaps,     {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_y,      incrovgaps,     {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_o,      incrovgaps,     {.i = -1 } },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY,                       XK_q,      killclient,     {0} },
-    { MODKEY,		            	XK_w,	   spawn,		   SHCMD("$BROWSER") },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY|ShiftMask,             XK_t,      setlayout,      {.v = &layouts[3]} },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
+    { MODKEY,			            XK_minus,  spawn,	       SHCMD("amixer -c 0 -- sset Master playback 1dB-;") },
+    { MODKEY|ShiftMask,			    XK_minus,  spawn,	       SHCMD("amixer -c 0 -- sset Master playback 5dB-;") },
+    { MODKEY,			            XK_equal,  spawn,	       SHCMD("amixer -c 0 -- sset Master playback 1dB+;") },
+    { MODKEY|ShiftMask,			    XK_equal,  spawn,	       SHCMD("amixer -c 0 -- sset Master playback 5dB+;") },
+    { MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
@@ -132,7 +149,6 @@ static Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 };
 
 /* button definitions */
